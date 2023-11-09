0bfd43f4eba95c2ff407647cc71105c52e9bf347
devbella <bellachoi1210@gmail.com>
2023-11-09 15:34:42 +0900

    fin

---
+++ src/components/CalNumberBtn.jsx
@@ -0,0 +1,16 @@
+const CalNumberBtn = ({ result, setResult, number }) => {
+  const onClickNumber = () => {
+    if (result == "0") {
+      setResult(number);
+    } else {
+      setResult(result + number);
+    }
+  };
+  return (
+    <button className="btn-style" onClick={onClickNumber}>
+      {number}
+    </button>
+  );
+};
+
+export default CalNumberBtn;
